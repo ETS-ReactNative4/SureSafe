846afcfbfbbe670f9fd3440ac39d6fceeae0218c
jericoxnavarro <jericoxnavarro@gmail.com>
2021-04-12 16:35:49 +0800

    🌱  Update Styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,7 @@
 import * as Colors from './Colors';
 import * as Fonts from './Fonts';
+import * as Padding from './Padding';
+import * as Defaults from './Defaults';
 import Images from './Images';
 
-export {Colors, Fonts, Images};
+export {Colors, Fonts, Images, Padding, Defaults};
