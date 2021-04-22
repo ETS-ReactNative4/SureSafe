4d05f3fa6ee2b548809675adc254bf13ce305d35
Jerico Navarro <38254143+jericoxnavarro@users.noreply.github.com>
2021-04-23 00:49:55 +0800

    🌱  Add Alert components

---
+++ src/components/Alert.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Modal} from 'react-native';
+
+import {Colors, Fonts} from '../styles';
+
+export default Alert = props => {
+  const {title, info, color, status, setStatus} = props;
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={status}
+      onRequestClose={() => setStatus()}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          paddingVertical: 30,
+        }}>
+        <View
+          style={{
+            width: '90%',
+            height: 200,
+            backgroundColor: Colors.PRIMARY,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 20,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.22,
+            shadowRadius: 2.22,
+            elevation: 3,
+          }}>
+          <Text style={[Fonts.H3, {marginTop: 'auto', color: color}]}>
+            {title}
+          </Text>
+          <Text
+            style={[
+              Fonts.BODY,
+              {
+                marginTop: 'auto',
+                width: '100%',
+                paddingHorizontal: 60,
+                textAlign: 'center',
+              },
+            ]}>
+            {info}
+          </Text>
+          <TouchableOpacity
+            onPress={() => setStatus(!status)}
+            style={{
+              width: '100%',
+              paddingVertical: 15,
+              backgroundColor: color,
+              alignItems: 'center',
+              marginTop: 'auto',
+              borderBottomStartRadius: 20,
+              borderBottomEndRadius: 20,
+            }}>
+            <Text style={[Fonts.H5, {color: Colors.PRIMARY}]}>OK</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
