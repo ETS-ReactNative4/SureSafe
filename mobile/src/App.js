1ad2119bb7fa4efd1b69fba42a2a0ce83495ab3d
jericoxnavarro16 <jericoxnavarro16@gmail.com>
2021-05-05 23:17:31 +0800

    🖥  Added Provider to APP

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -2,6 +2,8 @@ import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 import RootStack from './routes/RootStack';
 
@@ -11,10 +13,12 @@ export default App = () => {
   }, []);
 
   return (
-    <NavigationContainer>
-      <View style={{flex: 1}}>
-        <RootStack />
-      </View>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <View style={{flex: 1}}>
+          <RootStack />
+        </View>
+      </NavigationContainer>
+    </Provider>
   );
 };
